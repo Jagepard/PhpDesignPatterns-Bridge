1689d6d1e58dc6b162ffd73d3a5dfd2c94f835e8
jagepard <dankorot@gmail.com>
2020-08-14 04:06:36 -0700

    Update

--- tests/BridgeTest.php
+++ tests/BridgeTest.php
@@ -3,28 +3,26 @@
 declare(strict_types=1);
 
 /**
- * @author    : Korotkov Danila <dankorot@gmail.com>
- * @license   https://mit-license.org/ MIT
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
  */
 
 namespace Structural\Bridge\Tests;
 
-use Structural\Bridge\JsonOutput;
-use Structural\Bridge\OutputBridge;
-use Structural\Bridge\StringOutput;
+use Structural\Bridge\{Lowercase, ServiceBridge, Uppercase};
 use PHPUnit\Framework\TestCase as PHPUnit_Framework_TestCase;
 
 class BridgeTest extends PHPUnit_Framework_TestCase
 {
     public function testStringOutput()
     {
-        $service = new OutputBridge(new StringOutput());
-        $this->assertEquals('String', $service->doSomething('String'));
+        $service = new ServiceBridge(new Uppercase());
+        $this->assertEquals("STRING", $service->doSomething("String"));
     }
 
     public function testIntOutput()
     {
-        $service = new OutputBridge(new JsonOutput());
-        $this->assertEquals('"String"', $service->doSomething('String'));
+        $service = new ServiceBridge(new Lowercase());
+        $this->assertEquals("string", $service->doSomething("String"));
     }
 }
